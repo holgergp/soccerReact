d0273750cdab57905b6dd59627a6379dc4c524d1
xd98714 <holger.grosse-plankermann@codecentric.de>
2015-11-27 15:24:16 +0100

    - Rollback ctd

--- test/helpers/shallowRenderHelper.js
+++ test/helpers/shallowRenderHelper.js
@@ -5,7 +5,7 @@
  * @see http://simonsmith.io/unit-testing-react-components-without-a-dom/
  * @author somonsmith
  */
-import React from '../../../../concertReact/node_modules/react/addons';
+import React from 'react/addons';
 const TestUtils = React.addons.TestUtils;
 
 /**
