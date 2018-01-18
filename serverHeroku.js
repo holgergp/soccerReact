0c750b8e139621ac793f1c5e21924bd13a1b1d3a
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-01-18 18:22:30 +0100

    Starting anew using Create-React-App

--- serverHeroku.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-
-
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 5000);
--- src/components/run.js
+++
@@ -1,8 +0,0 @@
-import ReactDom from 'react-dom';
-/*eslint-disable no-unused-vars*/
-import React from 'react'
-  /*eslint-enable no-unused-vars*/;
-import App from './App';
-
-// Render the main component into the dom
-ReactDom.render(<App />, document.getElementById('app'));
